f9eecf7bf5765e60983c4bb1afefdb71a7996046
Javier Alejandro Garcia <garcia.javier.alejandro@gmail.com>
2025-12-18 14:23:13 -0300

    example of secrets.h

---
+++ firmware/include/secrets (example).h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define WIFI_SSID "1234"
+#define WIFI_PASS "0123456789"
+
+#define MQTT_USER "ESP32-01"
+#define MQTT_PASS "1234"
+
+// Cloudflare History API (secreto)
+#define CF_API_BASE_URL "https://iot-5wo.pages.dev"
+#define CF_API_KEY      "1234"
